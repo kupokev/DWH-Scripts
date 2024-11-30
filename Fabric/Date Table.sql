c0dcfc39ff0a149b72e127691b3defa17d69d416
Kevin <kupo.kev@gmail.com>
2024-11-30 12:11:35 -0600

    Update Date Table.sql
    finally fixed the naming of columns;
    added columns sequential_day, sequential_week, and sequential_month to make date manipulation a bit easier;

--- Fabric/Date Table.sql
+++ Fabric/Date Table.sql
@@ -1,13 +1,14 @@
-ALTER   PROCEDURE [dbo].[Initialize_DateTable]
+CREATE PROCEDURE [dbo].[Initialize_DateTable]
 AS
 BEGIN
 	SET NOCOUNT ON;
-
--- Set variables
+	
+	-- Set variables
 	DECLARE @StartDate DATE  = '2020-01-01'; -- Start-date
 	DECLARE @EndDate DATE =  CAST(YEAR(GETDATE()) + 2 AS CHAR(4)) + '-01-01'; -- End-Date
 	DECLARE @days INT = DATEDIFF(day, @StartDate, @EndDate);
-
+	DECLARE @offset INT = 7 - DATEPART(DW, @StartDate);
+	
 	DECLARE @DefaultDate DATE = '1900-01-01';
 	DECLARE @CurrentDate DATE;
 
@@ -22,132 +23,141 @@ BEGIN
 			[observed_date] [datetime2](0) NOT NULL,
 			[description] [varchar](32) NULL,
 			[created_date] [datetime2](0) NOT NULL,
-			[modified_date] [datetime2](0) NULL,
---			CONSTRAINT [PK_dbo_holiday] PRIMARY KEY CLUSTERED ([holiday_key] ASC)
+			[modified_date] [datetime2](0) NULL
 		);
 	END
 
--- Drop table if it already exists
+	-- Drop table if it already exists
     IF EXISTS (SELECT * FROM sysobjects WHERE name= N'Date' AND XTYPE='U')
 		DROP TABLE [dbo].[Date];
--- Create new table
 
+	-- Create new table
     CREATE TABLE [dbo].[Date](
-        [FullDate] [date] NOT NULL,
-		[DayOfWeek] [int] NULL ,
-		[DayOfWeekName] [varchar](10) NULL,
-		[DayOfWeekAbbrev] [varchar](5) NULL,
-		[DayOfMonth] [int] NULL,
-		[DayOfQuarter] [int] NULL,
-		[DayOfYear] [int] NULL,
-		[WeekOfYear] [int] NULL,
-		[MonthOfYear] [int] NULL,
-		[MonthName] [varchar](10) NULL,
-		[MonthNameAbbrev] [varchar](5) NULL,
-		[QuarterOfYear] [int] NULL,
-		[Year] [int] NULL,
-		[FiscalDayOfMonth] [int] NULL,
-		[FiscalDayOfQuarter] [int] NULL,
-		[FiscalDayOfYear] [int] NULL,
-		[FiscalWeekOfYear] [int] NULL,
-		[FiscalMonthOfYear] [int] NULL,
-		[FiscalPeriod] [int] NULL,
-		[FiscalQuarterOfYear] [int] NULL,
-		[FiscalYear] [int] NULL,
-		[IsBusinessDay] [bit] NULL,
-		[IsWeekday] [bit] NULL,
-		[IsHoliday] [bit] NULL,
-		[YYYYMM] [char](6) NULL,
-		[YYYYMMDD] [char](8) NULL,
-		[MMMYYYY] [char](9) NULL,
-		[MMYYYY] [char](7) NULL,
-		[YYYYQQ] [char](7) NULL,
-		[YYYYFQ] [char](7) NULL
-);
+        [full_date] [date] NOT NULL,
+		[sequential_day] [bigint] NOT NULL,
+		[sequential_week] [bigint] NOT NULL,
+		[sequential_month] [bigint] NOT NULL,
+		[day_of_week] [int] NULL ,
+		[day_of_week_name] [varchar](10) NULL,
+		[day_of_week_abbrev] [varchar](5) NULL,
+		[day_of_month] [int] NULL,
+		[day_of_quarter] [int] NULL,
+		[day_of_year] [int] NULL,
+		[week_of_year] [int] NULL,
+		[month_of_year] [int] NULL,
+		[month_name] [varchar](10) NULL,
+		[month_name_abbrev] [varchar](5) NULL,
+		[quarter_of_year] [int] NULL,
+		[year] [int] NULL,
+		[fiscal_day_of_month] [int] NULL,
+		[fiscal_day_of_quarter] [int] NULL,
+		[fiscal_day_of_year] [int] NULL,
+		[fiscal_week_of_year] [int] NULL,
+		[fiscal_month_of_year] [int] NULL,		
+		[fiscal_quarter_of_year] [int] NULL,
+		[fiscal_year] [int] NULL,
+		[is_business_day] [bit] NULL,
+		[is_weekday] [bit] NULL,
+		[is_holiday] [bit] NULL,
+		[yyyymm] [char](6) NULL,
+		[yyyymmdd] [char](8) NULL,
+		[mmmyyyy] [char](9) NULL,
+		[mmyyyy] [char](7) NULL,
+		[yyyyqq] [char](7) NULL,
+		[yyyyfq] [char](7) NULL
+	);
 
--- Build new date table data
+	-- Build new date table data
 	INSERT INTO [dbo].[Date] (
-		  [FullDate]
-		, [DayOfWeek]
-		, [DayOfWeekName]
-		, [DayOfWeekAbbrev]
-		, [DayOfMonth]
-		, [DayOfQuarter]
-		, [DayOfYear]
-		, [WeekOfYear]
-		, [MonthOfYear]
-		, [MonthName]
-		, [MonthNameAbbrev]
-		, [QuarterOfYear]
-		, [Year]
-		, [FiscalDayOfMonth]
-		, [FiscalDayOfQuarter]
-		, [FiscalDayOfYear]
-		, [FiscalWeekOfYear]
-		, [FiscalMonthOfYear]
-		, [FiscalQuarterOfYear]
-		, [FiscalYear]
-		, [IsBusinessDay]
-		, [IsWeekday]
-		, [IsHoliday]
-		, [YYYYMM]
-		, [YYYYMMDD]
-		, [MMMYYYY]
-		, [MMYYYY]
-		, [YYYYQQ]
-		, [YYYYFQ]
+		  [full_date]
+		, [sequential_day]
+		, [sequential_week]
+		, [sequential_month]
+		, [day_of_week]
+		, [day_of_week_name]
+		, [day_of_week_abbrev]
+		, [day_of_month]
+		, [day_of_quarter]
+		, [day_of_year]
+		, [week_of_year]
+		, [month_of_year]
+		, [month_name]
+		, [month_name_abbrev]
+		, [quarter_of_year]
+		, [year]
+		, [fiscal_day_of_month]
+		, [fiscal_day_of_quarter]
+		, [fiscal_day_of_year]
+		, [fiscal_week_of_year]
+		, [fiscal_month_of_year]		
+		, [fiscal_quarter_of_year]		
+		, [fiscal_year]
+		, [is_business_day]
+		, [is_weekday]
+		, [is_holiday]
+		, [yyyymm]
+		, [yyyymmdd]
+		, [mmmyyyy]
+		, [mmyyyy]
+		, [yyyyqq]
+		, [yyyyfq]
 	)
 		SELECT
-		  a.CurrDate AS [FullDate]
-		, DATEPART(DW, a.CurrDate) AS [DayOfWeek]
-		, DATENAME(DW, a.CurrDate) AS [DayOfWeekName]
+		  a.CurrDate AS [full_date]
+		, [sequential_day]
+		, (a.sequential_day + @offset - 1 - ((a.sequential_day + @offset - 1) % 7)) / 7 AS [sequential_week]
+		, DENSE_RANK() OVER(PARTITION BY 1 ORDER BY FORMAT(a.CurrDate, 'yyyyMM')) AS [sequential_month]
+		, DATEPART(DW, a.CurrDate) AS [day_of_week]
+		, DATENAME(DW, a.CurrDate) AS [day_of_week_name]
 		, CASE WHEN DATEPART(DW, a.CurrDate) IN (3, 5) THEN LEFT(DATENAME(DW, a.CurrDate), 4)
 			ELSE LEFT(DATENAME(DW, a.CurrDate), 3)
-			END AS [DayOfWeekAbbrev]
-		, DATEPART(DD, a.CurrDate) AS [DayOfMonth]
+			END AS [day_of_week_abbrev]
+		, DATEPART(DD, a.CurrDate) AS [day_of_month]
 		, DATEDIFF(DD,
 			DATEADD(qq, DATEDIFF(qq, 0, a.CurrDate), 0),
 			a.CurrDate) + 1
-			AS [DayOfQuarter]
-		, DATEPART(DY, a.CurrDate) AS [DayOfYear]
-		, DATEPART(WW, a.CurrDate) AS [WeekOfYear]
-		, DATEPART(MM, a.CurrDate) AS [MonthOfYear]
-		, DATENAME(MM, a.CurrDate) AS [MonthName]
+			AS [day_of_quarter]
+		, DATEPART(DY, a.CurrDate) AS [day_of_year]
+		, DATEPART(WW, a.CurrDate) AS [week_of_year]
+		, DATEPART(MM, a.CurrDate) AS [month_of_year]
+		, DATENAME(MM, a.CurrDate) AS [month_name]
 		, CASE WHEN DATEPART(MM, a.CurrDate) = 9 THEN LEFT(DATENAME(MM, a.CurrDate), 4)
 			ELSE LEFT(DATENAME(MM, a.CurrDate), 3)
-			END AS [MonthNameAbbrev]
-		, DATEPART(QQ, a.CurrDate) AS [QuarterOfYear]
-		, DATEPART(YY, a.CurrDate) AS [Year]
-		, DATEPART(DD, DATEADD(m, @FiscalDifference, a.CurrDate)) AS [FiscalDayOfMonth]
+			END AS [month_name_abbrev]
+		, DATEPART(QQ, a.CurrDate) AS [quarter_of_year]
+		, DATEPART(YY, a.CurrDate) AS [year]
+		, DATEPART(DD, DATEADD(m, @FiscalDifference, a.CurrDate)) AS [fiscal_day_of_month]
 		, DATEDIFF(DD,
 			DATEADD(qq, DATEDIFF(qq, 0, DATEADD(m, @FiscalDifference, a.CurrDate)), 0),
 			DATEADD(m, @FiscalDifference, a.CurrDate)) + 1
-			AS [FiscalDayOfQuarter]
-		, DATEPART(DY, DATEADD(m, @FiscalDifference, a.CurrDate)) AS [FiscalDayOfYear]
-		, DATEPART(WW, DATEADD(m, @FiscalDifference, a.CurrDate)) AS [FiscalWeekOfYear]
-		, DATEPART(MM, DATEADD(m, @FiscalDifference, a.CurrDate)) AS [FiscalMonthOfYear]
-		, DATEPART(QQ, DATEADD(m, @FiscalDifference, a.CurrDate)) AS [FiscalQuarterOfYear]
-		, DATEPART(YY, DATEADD(m, @FiscalDifference, a.CurrDate)) AS [FiscalYear]
+			AS [fiscal_day_of_quarter]
+		, DATEPART(DY, DATEADD(m, @FiscalDifference, a.CurrDate)) AS [fiscal_day_of_year]
+		, DATEPART(WW, DATEADD(m, @FiscalDifference, a.CurrDate)) AS [fiscal_week_of_year]
+		, DATEPART(MM, DATEADD(m, @FiscalDifference, a.CurrDate)) AS [fiscal_month_of_year]
+		, DATEPART(QQ, DATEADD(m, @FiscalDifference, a.CurrDate)) AS [fiscal_quarter_of_year]
+		, DATEPART(YY, DATEADD(m, @FiscalDifference, a.CurrDate)) AS [fiscal_year]
 		, CASE WHEN DATEPART(DW, a.CurrDate) = 1 OR DATEPART(DW, a.CurrDate) = 7 OR h.[holiday_key] IS NOT NULL THEN 0
 			ELSE 1
-			END AS [IsBusinessDay]
+			END AS [is_business_day]
 		, CASE WHEN DATEPART(DW, a.CurrDate) = 1 OR DATEPART(DW, a.CurrDate) = 7 THEN 0
 			ELSE 1
-			END AS [IsWeekday]
+			END AS [is_weekday]
 		, CASE WHEN h.[holiday_key] IS NULL THEN 0
 			ELSE 1
-			END AS [IsHoliday]
-		, LEFT(CONVERT(VARCHAR, a.CurrDate, 112), 6) AS [YYYYMM]
-		, CONVERT(VARCHAR, a.CurrDate, 112) AS [YYYYMMDD]
-		, CASE WHEN DATEPART(MM, a.CurrDate) = 9 THEN LEFT(DATENAME(MM, a.CurrDate), 4) ELSE LEFT(DATENAME(MM, a.CurrDate), 3) END + ' ' + CAST(DATEPART(YY, a.CurrDate) AS char(4)) AS [MMMYYYY]
-		, CASE WHEN MONTH(a.CurrDate) < 10 THEN '0' ELSE '' END + CAST(DATEPART(MM, a.CurrDate) AS varchar(4)) + '-' + CAST(DATEPART(YY, a.CurrDate) AS char(4)) AS [MMYYYY]
-		, CAST(DATEPART(YY, a.CurrDate) AS char(4)) + '/Q' + CAST(DATEPART(QQ, a.CurrDate) AS char(1)) AS [YYYYQQ]
-		, CAST(DATEPART(YY, DATEADD(m, @FiscalDifference, a.CurrDate)) AS char(4)) + '/Q' + CAST(DATEPART(QQ, DATEADD(m, @FiscalDifference, a.CurrDate)) AS char(1)) AS [YYYYFQ]
+			END AS [is_holiday]
+		, LEFT(CONVERT(VARCHAR, a.CurrDate, 112), 6) AS [yyyymm]
+		, CONVERT(VARCHAR, a.CurrDate, 112) AS [yyyymmdd]
+		, CASE WHEN DATEPART(MM, a.CurrDate) = 9 THEN LEFT(DATENAME(MM, a.CurrDate), 4) ELSE LEFT(DATENAME(MM, a.CurrDate), 3) END + ' ' + CAST(DATEPART(YY, a.CurrDate) AS char(4)) AS [mmmyyyy]
+		, CASE WHEN MONTH(a.CurrDate) < 10 THEN '0' ELSE '' END + CAST(DATEPART(MM, a.CurrDate) AS varchar(4)) + '-' + CAST(DATEPART(YY, a.CurrDate) AS char(4)) AS [mmyyyy]
+		, CAST(DATEPART(YY, a.CurrDate) AS char(4)) + '/Q' + CAST(DATEPART(QQ, a.CurrDate) AS char(1)) AS [yyyyqq]
+		, CAST(DATEPART(YY, DATEADD(m, @FiscalDifference, a.CurrDate)) AS char(4)) + '/Q' + CAST(DATEPART(QQ, DATEADD(m, @FiscalDifference, a.CurrDate)) AS char(1)) AS [yyyyfq]
     FROM (
-        SELECT DATEADD(dd, value - 1, @StartDate) AS CurrDate FROM GENERATE_SERIES(1, @days)
+        SELECT 
+			  DATEADD(dd, value - 1, @StartDate) AS CurrDate 
+			, value AS [sequential_day]
+		FROM GENERATE_SERIES(1, @days)
 	) a
 		LEFT OUTER JOIN [dbo].[Holiday] h ON a.CurrDate = h.observed_date;
 END
 
 GO
-
